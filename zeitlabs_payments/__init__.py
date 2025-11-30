4e9f1e261332cfe8a263cc7887bd89846afbbd9b
shadinaif <shadinaif@gmail.com>
2025-11-30 10:01:18 +0300

    chore: bump version to v0.1.4

--- zeitlabs_payments/__init__.py
+++ zeitlabs_payments/__init__.py
@@ -2,7 +2,7 @@
 Simple Ecommerce for Open edX payments.
 """
 
-__version__ = '0.1.2'
+__version__ = '0.1.4'
 
 
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
