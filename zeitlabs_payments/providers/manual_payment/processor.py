3d84fc47bbb3d557c6aacb6c88353abce08a6033
Deimer M <deimer.morales@edunext.co>
2026-02-27 12:54:56 -0500

    fix: adding correct function

--- zeitlabs_payments/providers/manual_payment/processor.py
+++ zeitlabs_payments/providers/manual_payment/processor.py
@@ -37,7 +37,7 @@ class ManualPaymentProcessor(BaseProcessor):
         """
         raise NotImplementedError
 
-    def process_payment(  # pylint: disable= too-many-positional-arguments
+    def payment_view(  # pylint: disable= too-many-positional-arguments
         self,
         request: Any,
         cart: Cart,
